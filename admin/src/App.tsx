5a6faee73669072173b4e254492ff3f33226e125
hprasath2306 <hariprasaths21cs@psnacet.edu.in>
2024-12-13 07:49:36 +0530

    initialise acadify

---
+++ admin/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1 className='font-bold text-2xl'>Admin Panel</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
